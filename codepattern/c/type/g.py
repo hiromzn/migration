3ae8b2b6e833d7a2592d2152618ebb23b9c2b83a
hiromzn <hiromzn@gmail.com>
2019-02-10 21:45:44 +0900

    mod

--- codepattern/c/type/g.py
+++ codepattern/c/type/g.py
@@ -3,6 +3,8 @@
 inf = open( 'typetable.csv', 'r' )
 
 for line in inf:
-	print( line, end='' )
+	#print( line, end='' )
+	array = line.strip().split(',')
+	print( 'kind:%s def:%s arg:%s' % tuple( array ) )
 
 print( 'END' )
