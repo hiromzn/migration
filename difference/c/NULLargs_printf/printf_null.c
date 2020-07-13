c1f637adf5af8e433f08770f7e5d63403850f104
Hiromichi Mizuno <hiromzn@gmail.com>
2020-07-13 11:50:38 +0900

    add null argument of printf()

---
+++ difference/c/NULLargs_printf/printf_null.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( void )
+{
+	char *str = NULL;
+	char *emp_str= "";
+
+	printf( "print NULL :%s\n", NULL );
+	printf( "print NULL str :%s\n", str );
+	printf( "print empty string \"\" :%s\n", emp_str );
+}
